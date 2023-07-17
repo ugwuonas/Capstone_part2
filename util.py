5d415e1997031022c094f68e439caadd5a0d0ae7
ugwuonas <s.ugwuonah@gmail.com>
2023-07-17 01:19:40 -0600

    updated util.py

--- util.py
+++ util.py
@@ -1,11 +1,8 @@
 import os
 import logging
-from dotenv import load_dotenv
 import boto3
 import psycopg2
 
-# Load environment variables from .env file
-load_dotenv()
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -13,12 +10,13 @@ logging.basicConfig(level=logging.INFO)
 def get_s3_connection():
     try:
        
-        s3 = boto3.resource(
+        s3 = boto3.client(
             service_name='s3',
             region_name='ca-central-1',
-            aws_access_key_id = os.environ['aws_access_key_id'],
-            aws_secret_access_key= os.environ['aws_secret_access_key']
+            aws_access_key_id = os.getenv("aws_access_key_id"),
+            aws_secret_access_key= os.getenv("aws_secret_access_key")
         )
+        
         logging.info("Connected to S3 successfully")
         return s3
     except Exception as e:
@@ -30,11 +28,11 @@ def get_redshift_connection():
     try:
         
         conn = psycopg2.connect(
-            host='redshift-cluster-2.cfz4dilotntk.ca-central-1.redshift.amazonaws.com',
+            host='redshift-cluster-1.casnx5izlkyy.ca-central-1.redshift.amazonaws.com',
             port=5439,
             database='dev',
-            user= os.environ['redshift_user'],
-            password= os.environ['redshift_password']
+            user= os.getenv("redshift_user"),
+            password= os.getenv("redshift_password")
         )
         logging.info("Connected to Redshift successfully")
         return conn
@@ -42,7 +40,6 @@ def get_redshift_connection():
     except Exception as e:
         logging.error(f"Error connecting to Redshift: {e}")
         return None
-
 
 
 
